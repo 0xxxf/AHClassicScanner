69b0858c8195fa3de38e10f2143192d289ac133c
Karol Ciechlecki <karolc9991@gmail.com>
2022-05-23 15:15:30 +0200

    implemented get_realm_list and some refactors

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from os import access
 import requests
 import json
 import csv
@@ -5,7 +6,7 @@ import bisect
 import sys
 from os.path import exists
 
-def get_data_from_request():
+def get_auctionhouse_data():
     """ Call Blizzard's API at us.api.blizzard.com/data/wow/connected-realm/{realm}/auctions/{auction}
 
     Args:
@@ -14,10 +15,7 @@ def get_data_from_request():
     Returns:
         status code of the request
     """
-    f = open("access_token.json")
-    data = json.load(f)
-    access_token = str(data['access_token'])
-
+    access_token = get_access_token()
     custom_params = {"namespace": "dynamic-classic-us",
                      "locale": "en_US", "access_token": access_token}
 
@@ -28,6 +26,21 @@ def get_data_from_request():
         json.dump(data, f)
     return res.status_code
 
+def get_realm_list():
+    access_token = get_access_token()
+    custom_params = {"namespace": "dynamic-classic-us", "locale": "en_US", "access_token": access_token}
+
+    res = requests.get("https://us.api.blizzard.com/data/wow/realm/index", params=custom_params)
+    for item in res.json()['realms']:
+      print(item['name'],item['id'])
+    return res.json()
+
+def get_access_token():
+    f = open("access_token.json")
+    data = json.load(f)
+    access_token = str(data["access_token"])
+    f.close()
+    return access_token
 
 def get_data_from_file():
     f = open("auction_data.json")
@@ -94,10 +107,11 @@ def copper_to_gsc(value):
 opts = [opt for opt in sys.argv[1:] if opt.startswith("-")]
 args = [arg for arg in sys.argv[1:] if not arg.startswith("-")]
 
+
 # If auction_data.json doesnt exist, update data 
 if not exists("auction_data.json"):
     print("Auction data not found, updating Data...")
-    data = get_data_from_request()
+    data = get_auctionhouse_data()
     if int(data) == 200:
         print("Data updated correctly")
     else:
@@ -105,12 +119,14 @@ if not exists("auction_data.json"):
 
 if "-u" in opts:
     # call blizz api and update our auction file
-    data = get_data_from_request()
+    data = get_auctionhouse_data()
     print("Request returned code: " + str(data))
     if int(data) == 200:
         print("Data updated and dumped into auction_data_json succesfully ")
     else:
         print("Data could not be updated")
+elif "-r" or "--realms" in opts:
+    get_realm_list()
 elif "-i" or "--item" in opts:
     price = get_buyout_price(str(args[0]))
     if price == 0:
@@ -118,5 +134,6 @@ elif "-i" or "--item" in opts:
     else:
         print("Last buyout price listed (Gold,Silver,Copper):")
         print(copper_to_gsc(int(price)))
+
 else:
     raise SystemExit()
